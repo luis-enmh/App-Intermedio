1100b0e4e027d9003c7aa0177dd51c10a1ad539d
Luis_ <luis_enmh@hotmail.com>
2023-10-05 16:31:32 -0600

    initial commit

---
+++ app/src/main/java/com/lemh/appintermedio/horoscoapp/ui/home/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.lemh.appintermedio.horoscoapp.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.lemh.appintermedio.R
+import com.lemh.appintermedio.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentcontainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
+    }
+}
